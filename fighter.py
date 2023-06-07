6358c183719f71ed7b4329420cdf4324d2db97c3
mk-ultra-victim <115838717+mk-ultra-victim@users.noreply.github.com>
2023-06-07 14:31:39 -0700

    Add files via upload

---
+++ fighter.py
@@ -0,0 +1,100 @@
+import pygame
+
+class Fighter():
+    def __init__(self, x, y, data, sprite_sheet, animation_steps):
+        self.size = data[0]
+        self.image_scale = data[1]
+        self.flip = False
+        self.animation_list = self.load_images(sprite_sheet, animation_steps)
+        # 0:idle 1:run 2:jump 3:attack_1 4:attack_2 5:hit 6:death
+        self.action = 0
+        self.frame_index = 0
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.rect = pygame.Rect((x, y, 80, 180))
+        self.vel_y = 0
+        self.jump = False
+        self.attacking = False
+        self.attack_type = 0
+        self.health = 100
+
+
+    def load_images(self, sprite_sheet, animation_steps):
+        # extract images from spritesheet
+        animation_list = []
+        for y, animation in enumerate(animation_steps):
+            temp_img_list = []
+            for x in range(animation):
+                temp_img = sprite_sheet.subsurface(x * self.size, y * self.size, self.size, self.size)
+                temp_img_list.append(pygame.transform.scale(temp_img, (self.size * self.image_scale, self.size * self.image_scale)))
+            animation_list.append(temp_img_list)
+        return animation_list
+        
+    def move(self, screen_width, screen_height, surface, target):
+        SPEED = 10
+        GRAVITY = 2
+        dx = 0
+        dy = 0
+
+        # get keypresses
+        key = pygame.key.get_pressed()
+
+        # can only perform other actions if not currently attacking
+        if self.attacking == False:
+            # movement
+            if key[pygame.K_a]:
+                dx = -SPEED
+            if key[pygame.K_d]:
+                dx = SPEED
+
+            # jump
+            if key[pygame.K_w] and self.jump == False:
+                self.vel_y = -30
+                self.jump = True
+
+            # attack
+            if key[pygame.K_r] or key[pygame.K_t]:
+                self.attack(surface, target)
+                # determine which attack type was used
+                if key[pygame.K_r]:
+                    self.attack_type = 1
+                if key[pygame.K_t]:
+                    self.attack_type = 2
+
+        # apply gravity
+        self.vel_y += GRAVITY
+        dy += self.vel_y
+
+        # ensure player stays on screen
+        if self.rect.left + dx < 0:
+            dx = -self.rect.left
+        if self.rect.right + dx > screen_width:
+            dx = screen_width - self.rect.right
+        if self.rect.bottom + dy > screen_height - 110:
+            self.vel_y = 0
+            self.jump = False
+            dy = screen_height - 110 - self.rect.bottom
+
+            
+        # ensure players face each other
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
+
+        # update player position
+        self.rect.x += dx
+        self.rect.y += dy
+
+        
+    def attack(self, surface, target):
+        self.attacking = True
+        attacking_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
+        if attacking_rect.colliderect(target.rect):
+            target.health -= 10
+
+
+        pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, (255, 0 , 0), self.rect)
+        surface.blit(self.image, (self.rect.x, self.rect.y))
---
+++ main.py
@@ -0,0 +1,94 @@
+import pygame
+from fighter import Fighter
+
+pygame.init()
+
+# create game window
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Brawler")
+
+# set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+# define colors
+RED = (255, 0 , 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# define fighter variables
+USAGI_SIZE = 1620
+USAGI_SCALE = 0.1
+USAGI_DATA = [USAGI_SIZE, USAGI_SCALE]
+
+# load background image
+bg_image = pygame.image.load("assets/images/background/bg_1_px.png").convert_alpha()
+
+# load spritesheets
+usagi_sheet = pygame.image.load("assets/images/usagi_spritesheet.png").convert_alpha()
+
+# define number of steps in each animation
+USAGI_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
+
+# function for drawing background
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0, 0))
+
+# load health bar images
+health_bar_red = pygame.image.load("assets/images/ui/health_bar_red.png").convert_alpha()
+health_bar_bg = pygame.image.load("assets/images/ui/health_bar_bg.png").convert_alpha()
+
+# function for drawing fighter health bars
+def draw_health_bar(health, x, y):
+    barx = 400
+    bary = 30
+
+    pygame.draw.rect(screen, BLACK, (x - 5, y - 5, barx + 10, bary + 10))
+    
+    ratio = health / 100
+    scaled_health_bar_bg = pygame.transform.scale(health_bar_bg, (barx, bary))
+    screen.blit(scaled_health_bar_bg, (x, y))
+    
+    scaled_health_bar_red = pygame.transform.scale(health_bar_red, (barx * ratio, bary))
+    screen.blit(scaled_health_bar_red, (x, y))
+    
+
+# create two instances of fighters
+fighter_1 = Fighter(200, 310, USAGI_DATA, usagi_sheet, USAGI_ANIMATION_STEPS)
+fighter_2 = Fighter(700, 310, USAGI_DATA, usagi_sheet, USAGI_ANIMATION_STEPS)
+
+# game loop
+run = True
+while run:
+
+    clock.tick(FPS)
+
+    # draw background
+    draw_bg()
+
+    # show player stats
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 580, 20)
+
+    # move fighters
+    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
+
+    # draw fighters
+    fighter_1.draw(screen)
+    fighter_2.draw(screen)
+
+    # event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    # update display
+    pygame.display.update()
+
+# exit pygame
+pygame.quit()
